d2d53d507bf0c6295cdf3ade8cbfdc8d4bda84ca
drehuwann <drehuwann@gmail.com>
2022-07-27 11:58:08 +1100

    adds checking (elem->data == nullptr) in ft_list_clear

--- c12/ex06/ft_list_clear.c
+++ c12/ex06/ft_list_clear.c
@@ -5,7 +5,11 @@
 void ft_list_clear(t_list *begin_list, void (*free_fct)(void *)) {
   while (begin_list) {
     t_list *nextelem = begin_list->next;
-    free_fct(begin_list->data);
+    void *data_toFree = begin_list->data;
+    if (data_toFree) {
+      free_fct(data_toFree);
+      data_toFree = (void *)0;
+    }
     free((void *)begin_list);
     begin_list = nextelem;
   }
