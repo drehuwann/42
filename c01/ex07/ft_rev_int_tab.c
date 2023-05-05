e4c4807cfb28a96d5897b4751f506119bee1c6db
drehuwann <drehuwann@gmail.com>
2023-05-05 23:38:29 +1100

    scope reduction in c01/ex07

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,10 +1,9 @@
 void ft_rev_int_tab(int *tab, int size)
 {
   int *end = &tab[size -1];
-  int tmp = 0;
   while ( end > tab )
     { //do swap
-      tmp = *tab;
+      int tmp = *tab;
       *tab++ = *end;
       *end-- = tmp;
     }
